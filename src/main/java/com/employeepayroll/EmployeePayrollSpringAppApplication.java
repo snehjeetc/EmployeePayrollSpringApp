5a1576af2c54a73a8e88ca85d53e4f11f3aba6ea
snehjeetc12 <csnehjeet12@gmail.com>
2021-05-02 18:17:11 +0530

    Added properties for different environment and added exception handler for EmployeePayrollException class

--- src/main/java/com/employeepayroll/EmployeePayrollSpringAppApplication.java
+++ src/main/java/com/employeepayroll/EmployeePayrollSpringAppApplication.java
@@ -2,16 +2,22 @@
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+import lombok.extern.slf4j.Slf4j;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //locolhost:8080/swagger-ui.html
 @EnableSwagger2
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollSpringAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollSpringAppApplication.class, args);
+		ConfigurableApplicationContext context = 
+				SpringApplication.run(EmployeePayrollSpringAppApplication.class, args);
+		log.info("Employee payroll app started in {} Environment", 
+				context.getEnvironment().getProperty("environment"));
 	}
 
 }
--- src/main/java/com/employeepayroll/exception/EmployeePayrollControllerValidation.java
+++ src/main/java/com/employeepayroll/exception/EmployeePayrollControllerValidation.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -17,17 +18,25 @@ import com.employeepayroll.dto.ResponseDTO;
 @ControllerAdvice
 public class EmployeePayrollControllerValidation extends ResponseEntityExceptionHandler{
 	
-	 @Override
-	    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-	                                                                  HttpHeaders headers,
-	                                                                  HttpStatus status,
-	                                                                  WebRequest request) {
-	       	List<String> errorMsgs = ex.getAllErrors()
-	       							   .stream()
-	       							   .map(error -> error.getDefaultMessage())
-	       							   .collect(Collectors.toList());
-	        return new ResponseEntity<>(
-	                new ResponseDTO(errorMsgs, "Encountered errors"),
-	                HttpStatus.BAD_REQUEST);
-	    }
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers,
+			HttpStatus status,
+			WebRequest request) {
+		List<String> errorMsgs = ex.getAllErrors()
+				.stream()
+				.map(error -> error.getDefaultMessage())
+				.collect(Collectors.toList());
+		return new ResponseEntity<>(
+				new ResponseDTO(errorMsgs, "Encountered errors"),
+				HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(EmployeePayrollException.class)
+	public ResponseEntity<ResponseDTO> handleEmployeePayrollException(
+					EmployeePayrollException exception){
+		ResponseDTO responseDto = new ResponseDTO("Exception while processing request", 
+				exception.getMessage());
+		return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
+	}
 }
